1871009ff7015a0ffd1f69d14412d1b8269fab66
Alejandro Rosales <alejandro.rosales@lasalle.mx>
2025-05-19 16:47:07 -0600

    Update fragment and vertex shaders: implement texture sampling and basic lighting calculations for improved rendering quality

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -2,16 +2,25 @@
 
 in vec3 FragPos;
 in vec3 Normal;
-in vec2 TexCoord; // Necesitas pasarlo desde el vertex shader
+in vec2 TexCoord;
 
-uniform vec3 color;
 uniform vec3 lightPos;
-uniform sampler2D baseColorTexture;
+uniform sampler2D texSampler;
 
 out vec4 FragColor;
 
 void main()
 {
+    vec3 color = texture(texSampler, TexCoord).rgb;
 
-    FragColor = vec4(color, 1.0);
+    float ambientStrength = 1.0;
+    vec3 ambient = ambientStrength * color;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * color;
+
+    vec3 result = ambient + diffuse;
+    FragColor = vec4(result, 1.0);
 }
